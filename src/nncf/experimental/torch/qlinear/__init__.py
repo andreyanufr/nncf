77a174df08603174b660ac3a0a326f69aba24273
Andrei Anufriev <andrey.anufriev@intel.com>
2026-04-22 14:26:39 +0200

    Fixed compression with group_size > 1.

--- src/nncf/experimental/torch/qlinear/__init__.py
+++ src/nncf/experimental/torch/qlinear/__init__.py
@@ -1,3 +1,14 @@
+# Copyright (c) 2026 Intel Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-
-from .qlinear import NNCFQLinear, create_nncf_qlinear, get_compressed_weight
+from .qlinear import NNCFQLinear as NNCFQLinear
+from .qlinear import create_nncf_qlinear as create_nncf_qlinear
+from .qlinear import get_compressed_weight as get_compressed_weight
